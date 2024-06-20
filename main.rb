af5c2c4952fc6c4c0f45ede9adacf909fa96df0b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2024-06-20 18:24:44 -0300

    chore: Refactor main.rb to use class structure for payment processors and services

--- main.rb
+++ main.rb
@@ -3,15 +3,19 @@ require_relative 'paypal_processor'
 require_relative 'credit_processor'
 require_relative 'payment_service'
 
-stripe_processor = StripeProcessor.new
-paypal_processor = PayPalProcessor.new
-credit_processor = CreditProcessor.new
+class Main
+  stripe_processor = StripeProcessor.new
+  paypal_processor = PayPalProcessor.new
+  credit_processor = CreditProcessor.new
 
-service = PaymentService.new(stripe_processor)
-puts service.make_payment(100)
+  service = PaymentService.new(stripe_processor)
+  puts service.make_payment(100)
 
-service = PaymentService.new(paypal_processor)
-puts service.make_payment(200)
+  service = PaymentService.new(paypal_processor)
+  puts service.make_payment(200)
 
-credit = PaymentService.new(credit_processor)
-puts credit.make_payment(300)
+  credit = PaymentService.new(credit_processor)
+  puts credit.make_payment(300)
+end
+
+Main.new
